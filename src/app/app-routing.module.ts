a9f8162a1fe7e37c834435c7980cf419155a0b4e
0625-nicolas <nicolas.gutierrez01@uceva.edu.co>
2025-05-20 11:55:47 -0500

    fixbug:correcion estructura de las carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/ova', pathMatch: 'full' },
+  { path: 'ova', loadChildren: () => import('./ova/ova.module').then(m => m.OvaModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
