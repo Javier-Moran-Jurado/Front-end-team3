e00f0b0e51da67b0f9ee9ef1eace90da1a986e49
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-05-20 14:15:19 -0500

    fix: correción enrutamiento home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {PageNoFoundComponent} from './page-no-found/page-no-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: "full"},
-  {path: '/home', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   /*{path: 'home', loadChildren: () =>
       import('./home/home.module').then(m => m.HomeModule)}*/
   {path: '**', component: PageNoFoundComponent}
