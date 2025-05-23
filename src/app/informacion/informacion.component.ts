498b5cf3e1f48bf5f7bb903fef8844f8f49580b0
0625-nicolas <nicolas.gutierrez01@uceva.edu.co>
2025-05-22 22:26:37 -0500

    fix: solucion error nombre carpetas

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -8,21 +8,6 @@ import { Component } from '@angular/core';
 export class InformacionComponent {
   teams = {
     team1: [
-      { name: 'NICOLAS', role: 'Scrum', img: 'assets/images/team1/nico.jpg' },
-      { name: 'SAAVEDRA', role: 'Software Architect', img: 'assets/images/team1/saavedra.jpg' },
-      { name: 'JUANES', role: 'Developer', img: 'assets/images/team1/juanes.jpg' },
-      { name: 'ARENAS', role: 'Developer', img: 'assets/images/team1/arenas.jpg' },
-      { name: 'MORALES', role: 'UX/UI Designer', img: 'assets/images/team1/morales.jpg' },
-    ],
-    team2: [
-      { name: 'VALENTINA', role: 'Scrum', img: 'assets/images/team2/valentina.jpg' },
-      { name: 'CAMILO', role: 'Software Architect', img: 'assets/images/team2/camilo.jpg' },
-      { name: 'STIVEN', role: 'Developer', img: 'assets/images/team2/stiven.jpg' },
-      { name: 'GUEVARA', role: 'Developer', img: 'assets/images/team2/guevara.jpg' },
-      { name: 'ANGELA', role: 'UX/UI Designer', img: 'assets/images/team2/angela.jpg' },
-      { name: 'JAVIER', role: 'Project Manager', img: 'assets/images/team2/javier.jpg'}
-    ],
-    team3: [
       { name: 'NICONICONI', role: 'Scrum', img: 'assets/img/niconiconi.jpg' },
       { name: 'SAAVEDRA', role: 'Software Architect', img: 'assets/img/saavedra.jpg' },
       { name: 'JUANES', role: 'Developer', img: 'assets/img/juanes.jpg' },
@@ -30,6 +15,23 @@ export class InformacionComponent {
       { name: 'ESCOPETA', role: 'UX/UI Designer', img: 'assets/img/escopeta.jpg' },
       { name: 'JAVIER EDUARDO', role: 'Project Manager', img: 'assets/img/javier.jpg'}
     ],
+
+    team2: [
+      { name: 'NICOLAS', role: 'Scrum', img: 'assets/images/team2/nico.jpg' },
+      { name: 'SAAVEDRA', role: 'Software Architect', img: 'assets/images/team2/saavedra.jpg' },
+      { name: 'JUANES', role: 'Developer', img: 'assets/images/team2/juanes.jpg' },
+      { name: 'ARENAS', role: 'Developer', img: 'assets/images/team2/arenas.jpg' },
+      { name: 'MORALES', role: 'UX/UI Designer', img: 'assets/images/team2/morales.jpg' },
+    ],
+    team3: [
+      { name: 'VALENTINA', role: 'Scrum', img: 'assets/images/team3/valentina.jpg' },
+      { name: 'CAMILO', role: 'Software Architect', img: 'assets/images/team3/camilo.jpg' },
+      { name: 'STIVEN', role: 'Developer', img: 'assets/images/team3/stiven.jpg' },
+      { name: 'GUEVARA', role: 'Developer', img: 'assets/images/team3/guevara.jpg' },
+      { name: 'ANGELA', role: 'UX/UI Designer', img: 'assets/images/team3/angela.jpg' },
+      { name: 'JAVIER', role: 'Project Manager', img: 'assets/images/team3/javier.jpg'}
+    ],
+
     team4: [
       { name: 'NICONICONI', role: 'Scrum', img: 'assets/img/niconiconi.jpg' },
       { name: 'SAAVEDRA', role: 'Software Architect', img: 'assets/img/saavedra.jpg' },
