ea8540b18c93425114e000e64dcae515d65f487a
Juan Diego <juan.rodriguez16@uceva.edu.co>
2025-05-26 09:40:13 -0500

    Se modifico el boton "operar"

---
+++ src/app/librerias/costos-presupuestos/lib-costos5/lib-costos5.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CostosRequest } from './costos-request.model';
+import { CostosService } from './costos.service';
+
+@Component({
+  selector: 'app-lib-costos5',
+  templateUrl: './lib-costos5.component.html',
+  styleUrls: ['./lib-costos5.component.css']
+})
+
+export class LibCostos5Component {
+  request: CostosRequest = {
+    costoFijo: 0,
+    costoVariable: 0,
+    costoIndirecto: 0,
+    unidades: 0,
+    margenGananciaPorcentaje: 0
+  };
+
+  resultado: any = null;
+
+  constructor(private costosService: CostosService) {
+  }
+
+  operar(): void {
+    console.log("Datos enviados:", this.request);
+    this.costosService.calcularCostos(this.request).subscribe({
+      next: (res) => {
+        console.log("Respuesta recibida:", res);
+        this.resultado = res;
+      },
+      error: (err) => {
+        console.error("Error en la petición:", err);
+        this.resultado = {error: 'No se pudo calcular.'};
+      }
+    });
+  }
+}
