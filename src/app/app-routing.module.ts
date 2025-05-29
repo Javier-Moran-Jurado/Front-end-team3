15974784c3f62b9fc0f436e22e01a314eb51d2e7
JAVIER EDUARDO MORAN JUARADO <javier.moran02@uceva.edu.co>
2025-05-29 17:03:22 -0500

    Guardando cambios locales antes de sincronizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FullLayoutComponent} from './template/full-layout/full-layout.component';
-import {HomeComponent} from './home/home.component';
+import { FullLayoutComponent } from './template/full-layout/full-layout.component';
+import { HomeComponent } from './home/home.component';
 import {HomeOvaComponent} from './home-ova/home-ova.component';
-import {LibEscitalaComponent} from './librerias/matematicas-discretas/lib-escitala/lib-escitala.component';
-import {LibGitcmdComponent} from './librerias/ambiente-web/lib-gitcmd/lib-gitcmd.component';
-import {LibCompilerComponent} from './librerias/arquitectura-computadores/lib-compiler/lib-compiler.component';
 import {InformacionComponent} from './informacion/informacion.component';
 import {OvaTemplateComponent} from './template/ova-template/ova-template.component';
 import {LibLagrangeComponent} from './librerias/analisis-numerico/lib-lagrange/lib-lagrange.component';
@@ -13,7 +10,6 @@ import {OvasComponent} from './ova/ovas/ovas.component';
 import {PageNoFoundComponent} from './page-no-found/page-no-found.component';
 import {EmptyLayoutComponent} from './template/empty-layout/empty-layout.component';
 import {LoginComponent} from './login/login.component';
-import {LibCostosXpComponent} from './librerias/costos-presupuestos/lib-costos-xp/lib-costos-xp.component';
 
 const routes: Routes = [
 
@@ -30,26 +26,22 @@ const routes: Routes = [
     component: OvaTemplateComponent,  // MOVER ESTE BLOQUE ARRIBA DE FullLayoutComponent
     children: [
       { path: 'home-ova', component: HomeOvaComponent },
-      {path: 'lib-costosxp', component: LibCostosXpComponent},
-      { path: 'lib-newton', component: LibLagrangeComponent },
-      { path: 'lib-escitala', component:LibEscitalaComponent},
-      { path: 'lib-gitcmd', component: LibGitcmdComponent},
-      { path: 'lib-compiler', component: LibCompilerComponent}
+      { path: 'lib-lagrange', component: LibLagrangeComponent },
+      { path: 'lib-newton', component: LibLagrangeComponent }
     ]
   },
   {
     path: '',
     component: FullLayoutComponent,
     children: [
-      { path: 'home', component: HomeComponent },
-      { path: 'informacion', component: InformacionComponent },
-      { path: '', component: PageNoFoundComponent},
-      { path: '', redirectTo: '/facultad', pathMatch: 'full' },
-      { path: 'facultad', loadChildren: () => import('./facultad/facultad.module').then(m => m.FacultadModule) },
+      {path: 'home', component: HomeComponent},
+      {path: 'informacion', component: InformacionComponent},
+      {path: '', component: PageNoFoundComponent},
+      {path: '', redirectTo: '/facultad', pathMatch: 'full'},
+      {path: 'facultad', loadChildren: () => import('./facultad/facultad.module').then(m => m.FacultadModule)},
       //{ path: '', redirectTo: '/home', pathMatch: 'full' }
-      { path: '', redirectTo: '/programas', pathMatch: 'full' },
-      { path: 'programas', loadChildren: () => import('./programas/programas.module').then(m => m.ProgramasModule) },
-      { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule) },
+      {path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)},
+      {path: 'programas', loadChildren: () => import('./programas/programas.module').then(m => m.ProgramasModule)},
     ]
   },
       {path:'**', component: PageNoFoundComponent}
@@ -61,4 +53,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
